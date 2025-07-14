c6b48683f361701c57360b4e03a921c6782c8d00
anirudhs-1010 <anirudhs1010@gmail.com>
2025-07-14 13:21:09 -0700

    Use SQL queries for Postgres

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,6 @@
 // PostgreSQL database configuration using Sequelize
 // Should:
-// - Import Sequelize and dotenv
-// - Configure Sequelize connection to PostgreSQL
+// - Use SQL queries
 // - Set up connection pool settings
 // - Handle connection errors and logging
-// - Export configured Sequelize instance
-// - Test database connection on startup 
+// - Test database connection on startup
